86d7b89ecb40bf3625a4eb522c3b9b593b78cb13
Mark Davidson <marko.davo@gmail.com>
2017-04-05 21:21:13 +1000

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System; 
+
+namespace StopwatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please type 'Start' to begin the stopwatch, 'Stop' to end or " +
+                "'exit' to quit: ");
+            var userInput = Console.ReadLine();
+
+            //my variable to run stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+            bool isStarted = false;
+            bool isAlreadyStarted = false; 
+
+            //If userr types exit the progarm will terminate
+            while (userInput.ToLower() != "exit")
+            {
+                if (userInput.ToLower() == "start")
+                {
+                    //catch mthod if the user types start twice in a row
+                    if (isAlreadyStarted)
+                    {
+                        throw new InvalidOperationException("You can't start the timer once it has been stated");
+                    }
+                    else
+                    {
+                        stopwatch.Start();
+                        isStarted = true;
+                        isAlreadyStarted = true; 
+                        Console.WriteLine("Now type 'stop' to end timer or 'start' to reset: ");
+                        userInput = Console.ReadLine();
+                    }
+                }
+                else if (userInput.ToLower() == "stop")
+                {
+                    if (isStarted)
+                    {
+                        stopwatch.Stop();
+                        Console.WriteLine("Now type 'stop' again for new time or 'start' to reset: ");
+                        userInput = Console.ReadLine();
+                        isAlreadyStarted = false; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("You need to start the timer first. Please type 'start'.");
+                        Console.Write("Now type start: ");
+                        userInput = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You did not type one of the required words. Please try again.");
+                    userInput = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Stopwatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StopwatchApp
+{
+    public class Stopwatch
+    {
+        DateTime startTime; 
+        DateTime endTime; 
+
+        public void Start()
+        {
+            startTime = DateTime.Now; 
+        }
+
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+            TimeSpan elpasedTime = endTime - startTime;
+            Console.WriteLine("The following "+elpasedTime+" has elpased");
+        }
+    }
+}
